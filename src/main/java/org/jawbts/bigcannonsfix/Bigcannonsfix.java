c9d77f85bb22c9e1d6e42c229df3315cc791416a
winsrewu <78122384+winsrewu@users.noreply.github.com>
2023-11-02 00:06:11 +0800

    0.0.1 first commit

---
+++ src/main/java/org/jawbts/bigcannonsfix/Bigcannonsfix.java
@@ -0,0 +1,13 @@
+package org.jawbts.bigcannonsfix;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Bigcannonsfix implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+        
+    }
+}
---
+++ src/main/java/org/jawbts/bigcannonsfix/mixin/YawControllerBlockEntityMixin.java
@@ -0,0 +1,33 @@
+package org.jawbts.bigcannonsfix.mixin;
+
+import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
+import com.simibubi.create.content.kinetics.transmission.sequencer.SequencerInstructions;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import rbasamoyai.createbigcannons.cannon_control.cannon_mount.CannonMountBlockEntity;
+import rbasamoyai.createbigcannons.cannon_control.cannon_mount.YawControllerBlockEntity;
+
+@Mixin(YawControllerBlockEntity.class)
+public abstract class YawControllerBlockEntityMixin extends KineticBlockEntity {
+    @Shadow(remap = false) public abstract @Nullable CannonMountBlockEntity getCannonMount();
+
+    public YawControllerBlockEntityMixin(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
+        super(typeIn, pos, state);
+    }
+
+    // overwrite
+    public void onSpeedChanged(float prevSpeed) {
+        if (sequenceContext != null
+                && sequenceContext.instruction() == SequencerInstructions.TURN_ANGLE) {
+            CannonMountBlockEntity cmbe = getCannonMount();
+            if (cmbe != null) {
+                float limit = (float) sequenceContext.getEffectiveValue(1);
+                cmbe.onSpeedChanged(limit + 1024f);
+            }
+        }
+    }
+}
