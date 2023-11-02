f6a831e7aff815225fa472ec516b5efeddad1939
WinsreWu <jerrywu3128@outlook.com>
2023-11-03 00:27:53 +0800

    0.0.1.3 yaw fix bug fix

--- src/main/java/org/jawbts/bigcannonsfix/mixin/YawControllerBlockEntityMixin.java
+++ src/main/java/org/jawbts/bigcannonsfix/mixin/YawControllerBlockEntityMixin.java
@@ -3,17 +3,15 @@ package org.jawbts.bigcannonsfix.mixin;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
 import com.simibubi.create.content.kinetics.transmission.sequencer.SequencerInstructions;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import rbasamoyai.createbigcannons.cannon_control.cannon_mount.CannonMountBlockEntity;
 import rbasamoyai.createbigcannons.cannon_control.cannon_mount.YawControllerBlockEntity;
 
 @Mixin(YawControllerBlockEntity.class)
 public abstract class YawControllerBlockEntityMixin extends KineticBlockEntity {
-    @Shadow(remap = false) public abstract @Nullable CannonMountBlockEntity getCannonMount();
 
     public YawControllerBlockEntityMixin(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
         super(typeIn, pos, state);
@@ -25,11 +23,12 @@ public abstract class YawControllerBlockEntityMixin extends KineticBlockEntity {
 
         if (sequenceContext != null
                 && sequenceContext.instruction() == SequencerInstructions.TURN_ANGLE) {
-            CannonMountBlockEntity cmbe = getCannonMount();
-            if (cmbe != null) {
-                float limit = (float) sequenceContext.getEffectiveValue(1);
-                cmbe.onSpeedChanged(limit + 1024f);
+            BlockEntity be = level.getBlockEntity(worldPosition.above());
+            if (!(be instanceof CannonMountBlockEntity cmbe)) {
+                return;
             }
+            float limit = (float) sequenceContext.getEffectiveValue(1);
+            cmbe.onSpeedChanged(limit + 1024f);
         }
     }
 }
